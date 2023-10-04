9d4da86a6d7b6d2756cda634e8395825544dca90
kurbaniyazov <admlbj23@gmail.com>
2023-10-05 01:24:48 +0600

    Initial commit

---
+++ cmd/api/main.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/gorilla/mux" // Import Gorilla Mux
+)
+
+const version = "1.0.0"
+
+type config struct {
+	Port int
+	Env  string
+}
+
+type application struct {
+	Config config
+	Logger *log.Logger
+}
+
+func main() {
+	var cfg config
+	flag.IntVar(&cfg.Port, "port", 4000, "API server port")
+	flag.StringVar(&cfg.Env, "env", "development", "Environment (development|staging|production)")
+	flag.Parse()
+
+	logger := log.New(os.Stdout, "", log.Ldate|log.Ltime)
+
+	app := &application{
+		Config: cfg,
+		Logger: logger,
+	}
+
+	// Create a Gorilla Mux router
+	r := mux.NewRouter()
+
+	// Register the /v1/healthcheck route using Gorilla Mux
+	r.HandleFunc("/v1/healthcheck", app.healthcheckHandler)
+
+	srv := &http.Server{
+		Addr:         fmt.Sprintf(":%d", cfg.Port),
+		Handler:      r, // Set the router as the handler
+		IdleTimeout:  time.Minute,
+		ReadTimeout:  10 * time.Second,
+		WriteTimeout: 30 * time.Second,
+	}
+
+	logger.Printf("starting %s server on %s", cfg.Env, srv.Addr)
+	err := srv.ListenAndServe()
+	logger.Fatal(err)
+}
