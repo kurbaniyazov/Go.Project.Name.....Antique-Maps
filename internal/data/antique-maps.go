523c9c5499561e38a525a20d20feb51bb82e07f7
kurbaniyazov <admlbj23@gmail.com>
2023-10-06 02:33:51 +0600

    Update internal/data/antique-maps.go
    Hiding struct fields in the JSON object

--- internal/data/antique-maps.go
+++ internal/data/antique-maps.go
@@ -5,13 +5,12 @@ import (
 )
 
 type Maps struct {
-	ID        int64     // Unique integer ID for the antique map
-	CreatedAt time.Time // Timestamp for when the antique map is added to our database
-	Title     string    // Antique map title
-	Year      int32     // Antique map creation year
-	Country   string    // Country of origin
-	Condition string    // Condition of the antique map (e.g., "Excellent," "Good," etc.)
-	Type      string    // Slice of types for the antique map
-	Version   int32     // The version number starts at 1 and will be incremented each
-	// time the antique map information is updated
+	ID        int64     `json:"id"`
+	CreatedAt time.Time `json:"created_at"`
+	Title     string    `json:"title"`
+	Year      int32     `json:"year"`
+	Country   string    `json:"country"`
+	Condition string    `json:"condition"`
+	Type      string    `json:"type"`
+	Version   int32     `json:"version"`
 }
