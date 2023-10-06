79cce2cb84fa5b47c184301d058a50518115867a
kurbaniyazov <admlbj23@gmail.com>
2023-10-06 15:47:49 +0600

    commit
    Customizing the Year field

--- internal/data/antique-maps.go
+++ internal/data/antique-maps.go
@@ -8,9 +8,9 @@ type Maps struct {
 	ID        int64     `json:"id"`
 	CreatedAt time.Time `json:"-"`
 	Title     string    `json:"title"`
-	Year      int32     `json:"year,omitempty"`
+	Year      Year      `json:"year,omitempty"`
 	Country   string    `json:"country"`
-	Condition string    `json:"condition"`
+	Condition string    `json:"condition,omitempty"`
 	Type      string    `json:"type"`
 	Version   int32     `json:"version"`
 }
