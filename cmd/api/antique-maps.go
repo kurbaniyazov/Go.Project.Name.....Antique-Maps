78fbdd032d0c8d89c69dba9079a607cc37c8d61d
kurbaniyazov <admlbj23@gmail.com>
2023-10-06 20:00:26 +0600

    commit
    ch 4.2.
    Making a bad request helper

--- cmd/api/antique-maps.go
+++ cmd/api/antique-maps.go
@@ -18,7 +18,7 @@ func (app *application) createAntiqueMapHandler(w http.ResponseWriter, r *http.R
 
 	err := app.readJSON(w, r, &input)
 	if err != nil {
-		app.errorResponse(w, r, http.StatusBadRequest, err.Error())
+		app.badRequestResponse(w, r, err)
 		return
 	}
 	fmt.Fprintf(w, "%+v\n", input)
