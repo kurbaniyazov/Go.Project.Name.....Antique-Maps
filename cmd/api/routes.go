9d25b4d54e04c73208b09a4e0189c895e7064e15
kurbaniyazov <admlbj23@gmail.com>
2023-10-31 21:47:44 +0600

    Chapter 11.1
    Global Rate Limiting

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -20,5 +20,5 @@ func (app *application) routes() http.Handler {
 
 	router.HandlerFunc(http.MethodPatch, "/v1/antique-maps/:id", app.updateAntiqueMapsHandler)
 	router.HandlerFunc(http.MethodDelete, "/v1/antique-maps/:id", app.deleteAntiqueMapsHandler)
-	return app.recoverPanic(router)
+	return app.recoverPanic(app.rateLimit(router))
 }
--- go.mod
+++ go.mod
@@ -6,4 +6,5 @@ require (
 	github.com/gorilla/mux v1.8.0 // indirect
 	github.com/julienschmidt/httprouter v1.3.0 // indirect
 	github.com/lib/pq v1.10.9 // indirect
+	golang.org/x/time v0.3.0 // indirect
 )
