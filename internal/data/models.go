7d24b6b8354273ad86d03b2aaf6609ba9763f449
kurbaniyazov <admlbj23@gmail.com>
2023-11-08 17:36:22 +0600

    Chapter 13.2
    Setting up the Users Model

--- internal/data/models.go
+++ internal/data/models.go
@@ -12,10 +12,12 @@ var (
 
 type Models struct {
 	AntiqueMaps AntiqueMapsModel
+	Users       UserModel
 }
 
 func NewModels(db *sql.DB) Models {
 	return Models{
 		AntiqueMaps: AntiqueMapsModel{DB: db},
+		Users:       UserModel{DB: db},
 	}
 }
