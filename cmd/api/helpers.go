03fbd89e5f077f628bf4a82258fee07423438d0e
kurbaniyazov <admlbj23@gmail.com>
2023-11-08 21:32:16 +0600

    Chapter 14.4
    Sending Background Emails

--- cmd/api/helpers.go
+++ cmd/api/helpers.go
@@ -111,3 +111,14 @@ func (app *application) readInt(qs url.Values, key string, defaultValue int, v *
 	}
 	return i
 }
+
+func (app *application) background(fn func()) {
+	go func() {
+		defer func() {
+			if err := recover(); err != nil {
+				app.Logger.PrintError(fmt.Errorf("%s", err), nil)
+			}
+		}()
+		fn()
+	}()
+}
