5c80b7aef7fc3909a798b069cb5e986740b4a7fb
kurbaniyazov <admlbj23@gmail.com>
2023-11-10 20:51:35 +0600

    Chapter 17.1
    Requiring User Activation

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -14,12 +14,11 @@ func (app *application) routes() http.Handler {
 
 	router.HandlerFunc(http.MethodGet, "/v1/healthcheck", app.healthcheckHandler)
 
-	router.HandlerFunc(http.MethodGet, "/v1/antique-maps", app.listAntiqueMapsHandler)
-	router.HandlerFunc(http.MethodPost, "/v1/antique-maps", app.createAntiqueMapHandler)
-	router.HandlerFunc(http.MethodGet, "/v1/antique-maps/:id", app.showAntiqueMapHandler)
-
-	router.HandlerFunc(http.MethodPatch, "/v1/antique-maps/:id", app.updateAntiqueMapsHandler)
-	router.HandlerFunc(http.MethodDelete, "/v1/antique-maps/:id", app.deleteAntiqueMapsHandler)
+	router.HandlerFunc(http.MethodGet, "/v1/antique-maps", app.requireActivatedUser(app.listAntiqueMapsHandler))
+	router.HandlerFunc(http.MethodPost, "/v1/antique-maps", app.requireActivatedUser(app.createAntiqueMapHandler))
+	router.HandlerFunc(http.MethodGet, "/v1/antique-maps/:id", app.requireActivatedUser(app.showAntiqueMapHandler))
+	router.HandlerFunc(http.MethodPatch, "/v1/antique-maps/:id", app.requireActivatedUser(app.updateAntiqueMapsHandler))
+	router.HandlerFunc(http.MethodDelete, "/v1/antique-maps/:id", app.requireActivatedUser(app.deleteAntiqueMapsHandler))
 
 	router.HandlerFunc(http.MethodPost, "/v1/users", app.registerUserHandler)
 	router.HandlerFunc(http.MethodPut, "/v1/users/activated", app.activateUserHandler)
