720b49c0b90105d2e0aa1f575bf12aed65e50a1a
kurbaniyazov <admlbj23@gmail.com>
2023-11-10 20:16:37 +0600

    Chapter 15.4
    Activating a User

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -22,5 +22,6 @@ func (app *application) routes() http.Handler {
 	router.HandlerFunc(http.MethodDelete, "/v1/antique-maps/:id", app.deleteAntiqueMapsHandler)
 
 	router.HandlerFunc(http.MethodPost, "/v1/users", app.registerUserHandler)
+	router.HandlerFunc(http.MethodPut, "/v1/users/activated", app.activateUserHandler)
 	return app.recoverPanic(app.rateLimit(router))
 }
