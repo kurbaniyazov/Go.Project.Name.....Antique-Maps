7f83227306f433073bd28a724957e6febd820e45
kurbaniyazov <admlbj23@gmail.com>
2023-11-10 20:36:03 +0600

    Chapter 16.3
    Authenticating Requests

--- internal/data/users.go
+++ internal/data/users.go
@@ -13,6 +13,7 @@ import (
 var (
 	ErrDuplicateEmail = errors.New("duplicate email")
 )
+var AnonymousUser = &User{}
 
 type User struct {
 	ID        int64     `json:"id"`
@@ -22,6 +23,10 @@ type User struct {
 	Password  password  `json:"-"`
 	Activated bool      `json:"activated"`
 	Version   int       `json:"-"`
+}
+
+func (u *User) IsAnonymous() bool {
+	return u == AnonymousUser
 }
 
 type password struct {
